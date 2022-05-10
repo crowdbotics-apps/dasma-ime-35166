576507bbc102d0a473d9af33130db3f47c643ed3
Crowdbotics <team@crowdbotics.com>
2022-05-10 23:58:26 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dasma_ime_35166/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dasma_ime_35166;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "dasma_ime_35166";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "dasma_ime_35166",
+  url: "https://dasma_ime_35166.botics.co",
+  api: "https://dasma_ime_35166.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
